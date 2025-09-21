fcd5bf1c54f25b62d4ffc9095f5603db73ceb947
rahmat.bizhan <rahmat.bizhan@afghan-wireless.com>
2025-09-21 15:36:36 +0430

    Database and backend setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
-import  useTheme  from "@/hooks/useTheme";
+import { api } from "@/convex/_generated/api";
+import useTheme from "@/hooks/useTheme";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme();
+
+
   return (
     <View
       style={styles.container}
@@ -11,7 +14,7 @@ export default function Index() {
       <Text>Hi</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle the mode</Text>
-        </TouchableOpacity>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ convex/todos.ts
@@ -0,0 +1,67 @@
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const getTodos = query({
+    handler: async (ctx) => {
+        const todos = await ctx.db.query("todos").order("desc").collect();
+        return todos;
+    }
+})
+
+export const addTodo = mutation({
+    args: {text:v.string()},
+    handler: async(ctx, args) => {
+        const todoId = await ctx.db.insert("todos",{
+            text: args.text,
+            isCompleted: false
+        });
+        return todoId;
+    },
+});
+
+export const toggleTodo = mutation({
+    args: {id:v.id("todos")},
+    handler: async(convexToJson,args) => {
+        const todo = await convexToJson.db.get(args.id)
+        if(!todo) throw new ConvexError("Todo not found")
+        
+            await convexToJson.db.patch(args.id, {
+                isCompleted: !todo.isCompleted
+            })
+    }
+})
+
+
+export const deleteTodo = mutation({
+    args: {id:v.id("todos")},
+    handler: async(convexToJson,args) => {
+      await convexToJson.db.delete(args.id);
+    }
+})
+
+
+export const updateTodo = mutation({
+    args: {
+        id: v.id("todos"),
+        text: v.string(),
+    },
+    handler: async(convexToJson,args) => {
+      await convexToJson.db.patch(args.id, {
+        text: args.text
+      });
+    }
+})
+
+
+export const clearAllTodos = mutation({
+    handler: async (convexToJson) => {
+        const todos = await convexToJson.db.query("todos").collect();
+
+        // Delete all todos
+        for (const todo of todos) {
+            await convexToJson.db.delete(todo._id)
+        }
+
+        return { deletedCount: todos.length};
+    },
+});
