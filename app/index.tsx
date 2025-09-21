d41a506c21e64521b730820658f919268d510810
rahmat.bizhan <rahmat.bizhan@afghan-wireless.com>
2025-09-21 09:09:26 +0430

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hi</Text>
+      <Link href={"/about"}>Visit About Screen</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+  },
+  content: {
+      fontSize: 30,
+  }
+})
